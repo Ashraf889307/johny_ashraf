22878c66b2f447ab0d14d33b28a27b5157bb0efa
Ashraf889307 <110244667+Ashraf889307@users.noreply.github.com>
2023-03-17 01:44:32 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rider_app/all_screens/login_screen.dart';
+import 'package:rider_app/all_screens/main_screen.dart';
+import 'package:rider_app/all_screens/register_screen.dart';
+import 'package:rider_app/data_handler/app_data.dart';
+
+import 'all_screens/allow_location.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+
+
+DatabaseReference driverRef =
+    FirebaseDatabase.instance.reference().child('drivers');
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'aaaa',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginScreen(),
+        initialRoute: FirebaseAuth.instance.currentUser == null
+            ? AllowLocation.idScreen
+            : MainScreen.idScreen,
+        routes:
+        {
+          RegisterScreen.idScreen: (context) => RegisterScreen(),
+          LoginScreen.idScreen: (context) => LoginScreen(),
+          MainScreen.idScreen: (context) => MainScreen(),
+          AllowLocation.idScreen: (context) => AllowLocation(),
+        },
+      ),
+    );
+  }
+}
